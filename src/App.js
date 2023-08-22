83509b866be9f240ba5e40bdb9acf9abf3d21b02
aharper8484 <aharper8484@gmail.com>
2023-08-22 09:40:44 +0100

    initial steps for enigma machine in react
    copied rotor values

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Header</p>
+        <p>Reflector</p>
+        <p>Slow Wheel</p>
+        <p>Medium Wheel</p>
+        <p>Fast Wheel</p>
+        <p>Static Wheel</p>
+        <p>Plugboard</p>
+        <p>Text for Encyption</p>
+        <p>Encryprion</p>
       </header>
     </div>
   );
