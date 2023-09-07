3447b3c3712a900faa10ad72cdfc125ece60a46f
aharper8484 <aharper8484@gmail.com>
2023-09-07 14:33:39 +0100

    early form added
    x

--- src/components/InputCode.js
+++ src/components/InputCode.js
@@ -1,9 +1,19 @@
+import { useState } from "react";
+
 function InputCode() {
+  const [encryption, setEncryption] = useState("");
+
   return (
     <div>
       <form>
         <label for="encryption">Code for Encryption:</label>
-        <input type="text" id="encryption" name="encryption" />
+        <input
+          type="text"
+          id="encryption"
+          name="encryption"
+          value={encryption}
+          onChange={(e) => setEncryption(e.target.value)}
+        />
       </form>
     </div>
   );
